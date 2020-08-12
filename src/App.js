057a048a200cb098164b54ad2463e33d2f56caa7
cgasper <gasperc1@gmail.com>
2020-08-12 11:56:41 -0400

    Initialize and build GuestList

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import GuestList from "./GuestList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    guests: [
+      {
+        name: "Treasure",
+        isConfirmed: false,
+      },
+      {
+        name: "Nick",
+        isConfirmed: true,
+      },
+      {
+        name: "Sam",
+        isConfirmed: false,
+      },
+    ],
+  };
+
+  getTotalInvited = () => this.state.guests.length();
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>RSVP</h1>
+          <form>
+            <input type="text" value="Safia" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">
+              Submit
+            </button>
+          </form>
+        </header>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who haven't responded
+            </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <GuestList guests={this.state.guests} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
